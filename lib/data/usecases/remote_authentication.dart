9a7856cbb59a5b070b1d1c10ff926d381a604549
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-15 01:59:21 -0300

    ansure HttpAdapter call post with correct URL

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -2,9 +2,9 @@ import 'package:meta/meta.dart';
 
 import '../../domain/entities/entities.dart';
 import '../../domain/helpers/helpers.dart';
-import '../../domain/usecases/authentication.dart';
+import '../../domain/usecases/usecases.dart';
 
-import '../http/http.dart';
+import '../http/http.dart'; 
 import '../models/models.dart';
 
 class RemoteAuthentication implements Authentication {
