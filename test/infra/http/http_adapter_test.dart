b6213f658254b49e1226531d828f1097d866892d
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-17 01:43:31 -0300

    ansure HttpAdapter throws Unauthorized on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -7,7 +7,6 @@ import 'package:fordev/infra/http/http.dart';
 
 import 'package:fordev/data/http/http.dart';
 
-
 class ClientSpy extends Mock implements Client {}
 
 void main() {
@@ -82,7 +81,6 @@ void main() {
       expect(response, null);
     });
 
-    
     test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400);
 
@@ -91,16 +89,21 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
-     
     test('Should return BadRequestError if post returns 400', () async {
-      mockResponse(400,body: '');
+      mockResponse(400, body: '');
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.badRequest));
     });
 
-     
+    test('Should return Unauthorized if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
 
