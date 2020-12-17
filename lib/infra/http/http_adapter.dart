825f479bc8d8ddc564534872ff4929612bcf90e5
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-17 12:57:43 -0300

    ansure HttpAdapter throws ServerError if post throws

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -17,11 +17,14 @@ class HttpAdapter implements HttpClient {
       'accept': 'application/json'
     };
 
-    
     final jsonBody = body != null ? jsonEncode(body) : null;
-    var response = Response('',500);
-    if (method == 'post') {
-      response = await client.post(url, headers: headers, body: jsonBody);
+    var response = Response('', 500);
+    try {
+      if (method == 'post') {
+        response = await client.post(url, headers: headers, body: jsonBody);
+      }
+    } catch (error) {
+      throw HttpError.serverError;
     }
     return _handleResponse(response);
   }
