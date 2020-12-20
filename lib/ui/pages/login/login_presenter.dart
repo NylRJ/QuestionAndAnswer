8456decabedff717a58d2cd943597e960f55d0ad
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-20 20:03:22 -0300

    make LoginPresenter auth async

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -2,11 +2,11 @@ abstract class LoginPresenter {
   Stream<String> get emailErrorStream;
   Stream<String> get passwordErrorStream;
   Stream<String> get mainErrorStream;
-  Stream<bool>   get isFormValidErrorStream;
-  Stream<bool>   get isLoadingErrorStream;
+  Stream<bool> get isFormValidErrorStream;
+  Stream<bool> get isLoadingErrorStream;
 
   void validateEmail(String email);
   void validatePassword(String password) {}
-  void auth() {}
-  void dispose() {}
+  Future<void> auth();
+  void dispose();
 }
