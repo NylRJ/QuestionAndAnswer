82b70425140dc4b81068da1d0b2a148c87a8a726
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-21 02:18:34 -0300

    move EmailInput to its own file

--- lib/ui/pages/login/login_pages.dart
+++ lib/ui/pages/login/login_pages.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import '../pages.dart';
 
 import '../../components/components.dart';
-import '../pages.dart';
+import 'components/components.dart';
 
 class LoiginPage extends StatefulWidget {
   final LoginPresenter presente;
@@ -35,15 +37,7 @@ class _LoiginPageState extends State<LoiginPage> {
           });
           widget.presente.mainErrorStream.listen((error) {
             if (error != null) {
-              Scaffold.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: Colors.red[900],
-                  content: Text(
-                    error,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              );
+              showErrorMessage(context,error);
             }
           });
 
@@ -57,65 +51,51 @@ class _LoiginPageState extends State<LoiginPage> {
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        StreamBuilder<String>(
-                            stream: widget.presente.emailErrorStream,
-                            builder: (context, snapshot) {
-                              return TextFormField(
-                                decoration: InputDecoration(
-                                    labelText: 'Email',
-                                    errorText: snapshot.data?.isEmpty == true
-                                        ? null
-                                        : snapshot.data,
-                                    icon: Icon(
-                                      Icons.email,
-                                      color:
-                                          Theme.of(context).primaryColorLight,
-                                    )),
-                                keyboardType: TextInputType.emailAddress,
-                                onChanged: widget.presente.validateEmail,
-                              );
-                            }),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8, bottom: 32),
-                          child: StreamBuilder<String>(
-                              stream: widget.presente.passwordErrorStream,
-                              builder: (context, snapshot) {
-                                return TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Senha',
-                                    errorText: snapshot.data?.isEmpty == true
-                                        ? null
-                                        : snapshot.data,
-                                    icon: Icon(
-                                      Icons.lock,
-                                      color:
-                                          Theme.of(context).primaryColorLight,
+                  child: Provider(
+                    create: (_) => widget.presente,
+                      child: Form(
+                      child: Column(
+                        children: [
+                          EmailInput(),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8, bottom: 32),
+                            child: StreamBuilder<String>(
+                                stream: widget.presente.passwordErrorStream,
+                                builder: (context, snapshot) {
+                                  return TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Senha',
+                                      errorText: snapshot.data?.isEmpty == true
+                                          ? null
+                                          : snapshot.data,
+                                      icon: Icon(
+                                        Icons.lock,
+                                        color:
+                                            Theme.of(context).primaryColorLight,
+                                      ),
                                     ),
-                                  ),
-                                  obscureText: true,
-                                  onChanged: widget.presente.validatePassword,
+                                    obscureText: true,
+                                    onChanged: widget.presente.validatePassword,
+                                  );
+                                }),
+                          ),
+                          StreamBuilder<bool>(
+                              stream: widget.presente.isFormValidErrorStream,
+                              builder: (context, snapshot) {
+                                return RaisedButton(
+                                  onPressed: snapshot.data == true
+                                      ? widget.presente.auth
+                                      : null,
+                                  child: Text('Entrar'.toUpperCase()),
                                 );
                               }),
-                        ),
-                        StreamBuilder<bool>(
-                            stream: widget.presente.isFormValidErrorStream,
-                            builder: (context, snapshot) {
-                              return RaisedButton(
-                                onPressed: snapshot.data == true
-                                    ? widget.presente.auth
-                                    : null,
-                                child: Text('Entrar'.toUpperCase()),
-                              );
-                            }),
-                        FlatButton.icon(
-                          onPressed: () {},
-                          icon: Icon(Icons.person),
-                          label: Text('Criar Conta'),
-                        ),
-                      ],
+                          FlatButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.person),
+                            label: Text('Criar Conta'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -127,3 +107,4 @@ class _LoiginPageState extends State<LoiginPage> {
     );
   }
 }
+
