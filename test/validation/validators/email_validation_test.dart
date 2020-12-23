bd1434fe31296572743014482b2d6ce4c37456c4
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-23 13:35:02 -0300

    ensure EmailValidation  return  null if email is empty

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,23 @@
+import 'package:test/test.dart';
+
+import 'package:fordev/validation/protocols/protocols.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main() {
+  EmailValidation sut;
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+  test('Should return  null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+}
