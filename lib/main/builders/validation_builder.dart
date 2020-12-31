31550c5b155fad6d6f836a610b7b466636501033
Moises R. Souza <loveloungeoficial@gmail.com>
2020-12-31 00:32:17 -0300

    Refactoring the Design Pattern Builder builder

--- lib/main/builders/validation_builder.dart
+++ lib/main/builders/validation_builder.dart
@@ -1,14 +1,16 @@
 import '../../validation/protocols/protocols.dart';
 import '../../validation/validators/validators.dart';
 
-
 class ValidationBuilder {
   static ValidationBuilder _instance;
   String fieldName;
   List<FieldValidation> validations = [];
 
+  //construtor privado
+  ValidationBuilder._();
+
   static ValidationBuilder field(String fieldName) {
-    _instance = ValidationBuilder();
+    _instance = ValidationBuilder._();
     _instance.fieldName = fieldName;
 
     return _instance;
